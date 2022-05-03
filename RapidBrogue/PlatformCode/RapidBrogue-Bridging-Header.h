aae9a1e0a7cfcacbef2adcb3c0680ae0191b04af
Bob Taylor <bob@taylor.md>
2022-05-02 21:57:36 -0700

    structuring for iOS app

---
+++ RapidBrogue/PlatformCode/RapidBrogue-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  RapidBrogue-Bridging-Header.h
+//  RapidBrogue
+//
+//  Created by Robert Taylor on 5/2/22.
+//
+
+#ifndef RapidBrogue_Bridging_Header_h
+#define RapidBrogue_Bridging_Header_h
+
+#import <SpriteKit/SpriteKit.h>
+#import "Rogue.h"
+#import "RogueDriver.h"
+#import "DirectionControlsViewController.h"
+
+#define kROWS        (30+3+1)
+#define kCOLS        100
+#define FONT_SIZE    16
+
+#endif /* RapidBrogue_Bridging_Header_h */
---
+++ RapidBrogue/PlatformCode/RogueMain.mm
@@ -0,0 +1,33 @@
+/*
+ *  RogueMain.c
+ *  Brogue
+ *
+ *  Created by Brian Walker on 12/26/08.
+ *  Copyright 2012. All rights reserved.
+ *
+ *  This file is part of Brogue.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Rogue.h"
+#include "IncludeGlobals.h"
+#include <time.h>
+
+
+void rogueMain() {
+    previousGameSeed = 0;
+    mainBrogueJunction();
+}
+
